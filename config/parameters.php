cd079a1cbfde47e6a33f66b430a7f22c324a652f
Izhar Baeza <ibaezar.1@gmail.com>
2021-02-21 05:15:38 -0300

    Se corrige intrucciones en index, controlador ErrorController y config/parameters para las redirecciones en caso de error.

--- config/parameters.php
+++ config/parameters.php
@@ -1,5 +1,5 @@
 <?php
 define("base_url", "http://localhost/php/12-Proyecto-tienda-web/");
-define("controller_default", "productoController");
+define("controller_default", "ProductoController");
 define("action_default", "index");
 ?>
--- controllers/ErrorController.php
+++ controllers/ErrorController.php
@@ -2,10 +2,6 @@
 class ErrorController{
 
     public function index(){
-        if(isset($_SESSION['admin'])){
-            require_once 'views/administrador/aside.php';
-        }
-
         require_once 'views/errores/404.php';
     }
 }
