711d245bee2a693f227862e0952d77e5793d25ea
Izhar Baeza <ibaezar.1@gmail.com>
2022-12-25 18:16:57 -0300

    principal slider template updated

--- views/producto/slider.php
+++ views/producto/slider.php
@@ -2,13 +2,23 @@
     <div class="col"></div>
     <div class="col-12 col-xl-10 col-slider">
         <?php $slider = Utils::getSlider() ?>
-        <div class="slider">
-            <div id="slider-principal" class="owl-carousel owl-theme">
-                <?php while($imagen = $slider->fetch_object()): ?>
-                <div class="item"><img src="<?=base_url?>uploads/slider/<?=$imagen->imagen?>" width="100%"></div>
-                <?php endwhile; ?>
+        <?php if(strlen($slider->fetch_object()) > 0): ?>
+            <div class="slider">
+                <div id="slider-principal" class="owl-carousel owl-theme">
+                    <?php while($imagen = $slider->fetch_object()): ?>
+                    <div class="item"><img src="<?=base_url?>uploads/slider/<?=$imagen->imagen?>" width="100%"></div>
+                    <?php endwhile; ?>
+                </div>
             </div>
-        </div>
+        <?php else: ?>
+            <div class="slider">
+                <div id="slider-principal" class="owl-carousel owl-theme">
+                    <div class="item"><img src="<?=base_url?>assets/img/slider-without.png" width="100%"></div>
+                    <div class="item"><img src="<?=base_url?>assets/img/slider-without.png" width="100%"></div>
+                    <div class="item"><img src="<?=base_url?>assets/img/slider-without.png" width="100%"></div>
+                </div>
+            </div>
+        <?php endif; ?>
     </div>
     <div class="col"></div>
 </div>
