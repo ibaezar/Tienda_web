e15e03ac34e2f64247020efd1d6c2b6d51613c8f
Izhar Baeza <ibaezar.1@gmail.com>
2022-12-25 18:40:11 -0300

    index of products template updated

--- views/producto/index.php
+++ views/producto/index.php
@@ -6,6 +6,7 @@
             <h2 class="title">Productos destacados</h2>
             <div class="row">
             <?php $productos = Utils::showProducts() ?>
+            <?php if(strlen($productos->fetch_object()) > 0): ?>
                 <?php while($produc = $productos->fetch_object()): ?>
                     <?php if($produc->stock > 0 && $produc->categoria_id != 3): ?>
                         <div class="col-sm-6 col-md-4 col-lg-3">
@@ -30,6 +31,17 @@
                         </div>
                     <?php endif; ?>
                 <?php endwhile; ?>
+            <?php else: ?>
+                <div class="col-12">
+                    <div class="alert alert-warning" role="alert">
+                    <h4 class="alert-heading">Ops...!</h4>
+                    <p>No existen productos creados en la base de datos.</p>
+                    <p>Te invitamos a acceder con tu cuenta de administrador para poder crear productos.</p>
+                    <hr>
+                    <p class="mb-0"><strong>Nota:</strong> Para poder crear productos, primero debes tener creado al menos una categoría.</p>
+                    </div>
+                </div>
+            <?php endif; ?>
             </div>
         </section>
     </div>
