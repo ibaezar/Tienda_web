782ffe2486e86ee1a6dc651200c65b3a7022b5f6
Izhar Baeza <ibaezar.1@gmail.com>
2022-12-25 18:26:12 -0300

    slider_marca template updated

--- views/producto/slider_marca.php
+++ views/producto/slider_marca.php
@@ -6,11 +6,20 @@
         <div class="slider">
             <div id="slider-marca" class="owl-carousel owl-theme">
                 <?php $marcas = Utils::showMarca() ?>
-                <?php while($marca = $marcas->fetch_object()): ?>
-                    <?php if($marca->nombre != 'Otro'): ?>
-                        <div class="item"><img class="img-marca" src="<?=base_url?>uploads/marcas/<?=$marca->ruta_imagen?>/<?=$marca->imagen?>"></div>
-                    <?php endif; ?>
-                <?php endwhile; ?>
+                <?php if(strlen($marcas->fetch_object()) > 0): ?>
+                    <?php while($marca = $marcas->fetch_object()): ?>
+                        <?php if($marca->nombre != 'Otro'): ?>
+                            <div class="item"><img class="img-marca" src="<?=base_url?>uploads/marcas/<?=$marca->ruta_imagen?>/<?=$marca->imagen?>"></div>
+                        <?php endif; ?>
+                    <?php endwhile; ?>
+                <?php else: ?>
+                    <div class="item"><img class="img-marca" src="<?=base_url?>assets/img/marca-without.png"></div>
+                    <div class="item"><img class="img-marca" src="<?=base_url?>assets/img/marca-without.png"></div>
+                    <div class="item"><img class="img-marca" src="<?=base_url?>assets/img/marca-without.png"></div>
+                    <div class="item"><img class="img-marca" src="<?=base_url?>assets/img/marca-without.png"></div>
+                    <div class="item"><img class="img-marca" src="<?=base_url?>assets/img/marca-without.png"></div>
+                    <div class="item"><img class="img-marca" src="<?=base_url?>assets/img/marca-without.png"></div>
+                <?php endif; ?>
             </div>
         </div>
     </div>
